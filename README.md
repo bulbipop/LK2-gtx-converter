9feec55dc744ea3f059f568dbe1fe30a9c2324af
bulbipop <fierbenj@gmail.com>
2021-02-10 09:47:37 +0100

    DXT1 decompression and file extraction

--- README.md
+++ README.md
@@ -1,26 +1,5 @@
-LK2-gtx-tex-converter
+.gtx → .png converter
 
-- [x] Convert .gtx/.tex to .png (Almost, method "desaturation" needs to be tweaked?)
-- [ ] Convert .png to .gtx/.tex
+Tested for Lost Kingdoms and Lost Kingdoms II and can extract gtx inside .tex, .pds, .ctm, .ptm
 
-Data:
-8 bytes fill 64 pixels.
-
-Pixels are filled in this order:  
- 1  2  3  4  17 18 19 20  65 66 ...  
- 5  6  7  8  21 22 23 24  
- 9 10 11 12  25 26 27 28  
-13 14 15 16  29 30 31 32  
-33 34 35 36  49 50 51 52  
-37 38 39 40  53 54 55 56  
-41 42 43 44  57 58 59 60  
-45 46 47 48  61 62 63 64  
-
-example with FF 6D 37 E4 A8 2F F8 0F  
-FF6D: first color: RGB565 (16-bit RGB)  
-37E4: second color: RGB565 (16-bit RGB)  
-A8 2F F8 0F needs to be converted to binary: 10 10 10 00 00 10 11 11 11 11 10 00 00 00 11 11 and represents the color of each pixels:  
-00: first color  
-01: second color  
-10: second color desaturated?  
-11: first color desaturated?  
+GTX are DXT1.
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import numpy as np
+from textwrap import wrap
 from PIL import Image
 from binascii import hexlify
 
@@ -14,89 +15,114 @@ class Color:
         self.r, self.g, self.b = r, g, b
 
     def __add__(self, other):
-        """ Blend the two colors together """
         new_r = (self.r + other.r) // 2
         new_g = (self.g + other.g) // 2
         new_b = (self.b + other.b) // 2
         return Color(new_r, new_g, new_b)
 
+    def __mul__(self, other):
+        new_r = (2 * self.r + other.r) // 3
+        new_g = (2 * self.g + other.g) // 3
+        new_b = (2 * self.b + other.b) // 3
+        return Color(new_r, new_g, new_b)
+
+    def blend(color1, color2, mode):
+        if mode == 0:
+            color = color1
+        elif mode == 1:
+            color = color2
+        elif mode == 2:
+            if color1 < color2:
+                color = color1 + color2
+            else:
+                color = color1 * color2
+        elif mode == 3:
+            color = color2 * color1
+        return color.to_rgb()
+
+    
     def __str__(self):
         return f'Color({self.r}, {self.g}, {self.b})'
 
     def to_rgb(self):
         return (self.r, self.g, self.b)
 
-    def desaturation(self):
-        """ TODO: find the right  """
-        return Color(int(self.r * 0.8), int(self.g * 0.8), int(self.b * 0.8))
+    def __lt__(self, other):
+        return self.to_rgb() < other.to_rgb()
 
 
-with open(sys.argv[1], 'rb') as f:
-    f.seek(0x4)
-    while hexlify(f.read(4)) != b'47545831':    # == GTX1
-        f.seek(f.tell() + 0x0C)
-    addr = f.tell()
-    WIDTH = addr + 4
-    HEIGHT = addr + 6
-    DATA = addr + 0x18
-    f.seek(WIDTH)
-    width = int(hexlify(f.read(2)), 16)
-    f.seek(HEIGHT)
-    height = int(hexlify(f.read(2)), 16)
-    f.seek(DATA)
 
-    if width % 4 != 0:
-        width += (width % 4) + 4
+def main(files, shows=False, saves=True):
+    for file in files:
+        with open(file, 'rb') as f:
+            addrs = []
+            f.seek(0x4)
+            while reader := hexlify(f.read(4)):    
+                if reader == b'47545831': # == GTX1
+                    addrs.append(f.tell())
 
-    x, y = 0, 0
-    second_line = False
-    img = np.zeros((height, width, 3), dtype=np.uint8)
-    while True:
-        try:
-            back_color = Color(int(hexlify(f.read(2)), 16))
-            front_color = Color(int(hexlify(f.read(2)), 16))
-            coloration = f'{int(hexlify(f.read(4)), 16):032b}'
-            for intensity, brush in zip(coloration[::2], coloration[1::2]):
-                print('now in', x, y)
-                if int(intensity):
-                    if int(brush):
-                        img[y][x] = back_color.desaturation().to_rgb()
+            for addr in addrs:
+                f.seek(addr)
+                WIDTH = addr + 4
+                HEIGHT = addr + 6
+                DATA = addr + 0x18
+                f.seek(WIDTH)
+                width = int(hexlify(f.read(2)), 16)
+                f.seek(HEIGHT)
+                height = int(hexlify(f.read(2)), 16)
+                f.seek(DATA)
+
+                if width % 4 != 0:
+                    width += (width % 4) + 4
+
+                x, y = 0, 0
+                second_line = False
+                img = np.zeros((height, width, 3), dtype=np.uint8)
+                while True:
+                    try:
+                        back_color = Color(int(hexlify(f.read(2)), 16))
+                        front_color = Color(int(hexlify(f.read(2)), 16))
+                        coloration = wrap(f'{int(hexlify(f.read(4)), 16):032b}', 2)
+                        for blend_mode in (int(c, 2) for c in coloration):
+                            img[y][x] = Color.blend(back_color, front_color, blend_mode)
+                                
+                            square_end_x = (x != 0 and (x+1) % 4 == 0)
+                            double_square_end_x = (x != 0 and (x+1) % 8 == 0)
+                            square_end_y = (y != 0 and (y+1) % 4 == 0)
+
+                            if x == width - 1 and square_end_y and second_line:
+                                x = 0
+                                y += 1
+                                second_line = False
+                            elif double_square_end_x and square_end_y:
+                                if second_line:
+                                    x += 1
+                                    y -= 7
+                                    second_line = False
+                                else:
+                                    x -= 7
+                                    y += 1
+                                    second_line = True
+                            elif square_end_x and square_end_y:
+                                x += 1
+                                y -= 3
+                            elif square_end_x:
+                                x -= 3
+                                y += 1
+                            else:
+                                x += 1
+                    except Exception as e:
+                        print(e)
+                        break
+                png = Image.fromarray(img, 'RGB')
+                if saves:
+                    if len(addrs) > 1:
+                        png.save(f'{file}_{addr}.png')
                     else:
-                        img[y][x] = front_color.desaturation().to_rgb()
-                else:
-                    if int(brush):
-                        img[y][x] = front_color.to_rgb()
-                    else:
-                        img[y][x] = back_color.to_rgb()
+                        png.save(f'{file}.png')
+                if shows:
+                    png.show()
 
-                square_end_x = (x != 0 and (x+1) % 4 == 0)
-                double_square_end_x = (x != 0 and (x+1) % 8 == 0)
-                square_end_y = (y != 0 and (y+1) % 4 == 0)
 
-                if x == width - 1 and square_end_y and second_line:
-                    x = 0
-                    y += 1
-                    second_line = False
-                elif double_square_end_x and square_end_y:
-                    if second_line:
-                        x += 1
-                        y -= 7
-                        second_line = False
-                    else:
-                        x -= 7
-                        y += 1
-                        second_line = True
-                elif square_end_x and square_end_y:
-                    x += 1
-                    y -= 3
-                elif square_end_x:
-                    x -= 3
-                    y += 1
-                else:
-                    x += 1
-        except Exception as e:
-            print(e)
-            break
-    png = Image.fromarray(img, 'RGB')
-    png.save(f'{sys.argv[1]}.png')
-    png.show()
+if __name__ == '__main__':
+    main(sys.argv[1:])
